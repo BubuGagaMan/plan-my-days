30c9692ee5aed1f5ae9ef4b380cf91566868b094
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-10-24 20:12:04 +0100

    update actions store

--- src/app/(frontend)/store/slices/actionsSettings/actionsSettings.ts
+++ src/app/(frontend)/store/slices/actionsSettings/actionsSettings.ts
@@ -2,19 +2,11 @@ import { StateCreator } from "zustand";
 import { UseAppStore } from "../../useAppStore";
 
 export interface ActionsSettingsSlice {
-    clear: boolean;
-    setClear: (checked: boolean) => void;
     overwrite: boolean;
     setOverwrite: (checked: boolean) => void;
 }
 
 export const createActionsSettingsSlice: StateCreator<UseAppStore, [], [], ActionsSettingsSlice> = (set) => ({
-    clear: false,
-
-    setClear: (checked) => {
-        console.log(checked)
-        set({clear: checked})
-    },
     overwrite: false,
 
     setOverwrite: (checked) => {
