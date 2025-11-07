99651a934fda087050db55ad8ff705102bf3f655
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-11-07 23:45:17 +0000

    update the date types v1

--- src/app/types/dateTypes.ts
+++ src/app/types/dateTypes.ts
@@ -1,11 +1,3 @@
-export type DBDay = {
-    id?: string;
-    off_date: string;
-    user_id?: string;
-    notes?: string | null;
-    publicHoliday?: string | null;
-};
-
 export type DBDayMark = {
     id: string;
     title: string;
@@ -27,24 +19,20 @@ export type PublicHoliday = {
     date: string;
 };
 
-export type PublicHolidaySeparateDMY = {
-    date: number;
-    year: number;
+export type Day = {
+    weekDay: string;
+    monthDay: number;
     month: number;
-    title: string;
+    year: number;
+    isFromCurrentMonth: boolean;
+    date: string;
+    markedDay: DBMarkedDay | null;
+    publicHoliday: PublicHoliday | null;
 };
 
-export type Day = {
-    date: number;
-    day: number;
-    year: number;
-    month: number;
-    isWeekend?: boolean;
-    notes?: string | null;
-    publicHoliday?: PublicHolidaySeparateDMY | null;
-    dayMarkId?: string;
-    dayMark?: DBDayMark;
-};
+export type Month = Day[]
+
+export type Year = Month[]
 
 export type NumberToStringMap = {
     [key: number]: string;
@@ -57,3 +45,31 @@ export enum CalendarGrid {
 
 // creating a set is overkill for 2 items... initiating it will cost more
 export const CalendarGridArr = [CalendarGrid.MONTH, CalendarGrid.YEAR];
+
+
+// export type DBDay = {
+//     id?: string;
+//     off_date: string;
+//     user_id?: string;
+//     notes?: string | null;
+//     publicHoliday?: string | null;
+// };
+
+// export type PublicHolidaySeparateDMY = {
+//     date: number;
+//     year: number;
+//     month: number;
+//     title: string;
+// };
+
+// export type Day = {
+//     date: number;
+//     day: number;
+//     year: number;
+//     month: number;
+//     isWeekend?: boolean;
+//     notes?: string | null;
+//     publicHoliday?: PublicHolidaySeparateDMY | null;
+//     dayMarkId?: string;
+//     dayMark?: DBDayMark;
+// };
