93d6cdea16f3ae1306f0120f8b8ca2d931f0c686
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-11-20 22:17:15 +0000

    updating dayMarks Services

--- src/app/services/dayMarks.services.ts
+++ src/app/services/dayMarks.services.ts
@@ -1,9 +1,14 @@
 import { cache } from "react";
 import { serverSideSupabase } from "../(frontend)/supabaseServerShit/serverSideSupabase";
+import { getUserId } from "./user.services";
 
 export const getAllDayMarks = cache(async () => {
     const supabase = await serverSideSupabase();
-    const { data } = await supabase.from("day_mark").select("id, title, background_color, font_color");
+    const userId = await getUserId(supabase);
+    const { data } = await supabase
+        .from("day_mark")
+        .select("id, title, background_color, font_color")
+        .eq("user_id", userId);
 
     return data;
 });
